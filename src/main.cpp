7ce39b6523861611db4d45bbd0016802ca5b04a3
hwthinker <hwthinker@gmail.com>
2024-07-23 15:18:01 +0700

    update readme

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include <Arduino.h>
 
 // Definisikan pin LED
-#define LED_PIN1 2
-#define LED_PIN2 4
+#define LED_ESP 2
+#define RLY1 18
 
 void setup() {
   // Atur kedua pin sebagai output
-  pinMode(LED_PIN1, OUTPUT);
-  pinMode(LED_PIN2, OUTPUT);
+  pinMode(LED_ESP, OUTPUT);
+  pinMode(RLY1, OUTPUT);
 }
 
 void loop() {
   // Nyalakan LED pada pin GPIO2 dan matikan LED pada pin GPIO4
-  digitalWrite(LED_PIN1, HIGH);
-  digitalWrite(LED_PIN2, LOW);
+  digitalWrite(LED_ESP, HIGH);
+  digitalWrite(RLY1, HIGH);
+
   delay(1000); // Tunggu selama 1 detik
 
   // Matikan LED pada pin GPIO2 dan nyalakan LED pada pin GPIO4
-  digitalWrite(LED_PIN1, LOW);
-  digitalWrite(LED_PIN2, HIGH);
+  digitalWrite(LED_ESP, LOW);
+  digitalWrite(RLY1, LOW);
   delay(1000); // Tunggu selama 1 detik
 }
